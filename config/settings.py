4d89fee2b5452f1ab108c4ca4907b2528e97afd7
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-03-04 22:28:40 +0900

    設定値の一部を別ファイル空の読み出しに変更

--- config/settings.py
+++ config/settings.py
@@ -12,20 +12,24 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env()
+env.read_env(BASE_DIR / '.env') 
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wld67j#hraept@z!bo+u(%5%8gu#3z@s4wv3e*(j92^cjf6&6g'
+SECRET_KEY = env.str('SECRET_KEY', default="secret_key")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 
 # Application definition
@@ -74,10 +78,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default':env.db(),
 }
 
 
