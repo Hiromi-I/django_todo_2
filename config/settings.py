3b238d491a898056cfc8d5839b23a55db2d6ab0a
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-03-04 22:56:37 +0900

    動作確認で設定値を修正

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env()
-env.read_env(BASE_DIR / '.env') 
+env.read_env('.env') 
 
 
 # Quick-start development settings - unsuitable for production
