f0c2454fcfd8a99759dd8801ade353c5eac36b39
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-03-05 22:39:33 +0900

    ホーム画面を仮実装

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import LoginView
+from .views import LoginView, HomeViw
 
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
+    path('home/', HomeViw.as_view(), name='home'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
 from django.contrib.auth.views import LoginView as _LoginView
+from django.views.generic import TemplateView
 
 
 class LoginView(_LoginView):
     template_name = 'accounts/login.html'
+
+
+class HomeViw(TemplateView):
+    template_name = 'accounts/home.html'
