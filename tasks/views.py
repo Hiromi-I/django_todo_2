0c4417465f4e815fa1886705a74c0105b3a0ffcb
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-03-06 23:43:05 +0900

    タスク一覧画面を仮作成

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class TasksListView(TemplateView):
+    template_name = "tasks/list.html"
