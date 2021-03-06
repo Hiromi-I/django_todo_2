f342dc9d6f5a07ddc42702a53d1bbf20b19d2cab
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-03-06 23:32:41 +0900

    Taskモデルを作成

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField("タイトル", max_length=100)
+    description = models.TextField("詳細")
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    is_complete = models.BooleanField("完了済み")
+    created_at = models.DateTimeField("作成日時", auto_now_add=True)
+    updated_at = models.DateTimeField("更新日時", auto_now=True)
+
+    def __str__(self):
+        return f"[{self.author}] {self.title}"
