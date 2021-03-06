a48998d775edf7da4af024943a7fad6a282f1e63
Hiromi Iwai <hiromi.iwai.04.03@gmail.com>
2021-03-06 23:46:30 +0900

    タスク作成画面を仮作成

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TasksListView
+from .views import TasksListView, TasksCreateView
 
 
 app_name = "tasks"
 
 urlpatterns = [
     path("list/", TasksListView.as_view(), name="list"),
+    path("create/", TasksCreateView.as_view(), name="create"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class TasksListView(TemplateView):
     template_name = "tasks/list.html"
+
+
+class TasksCreateView(TemplateView):
+    template_name = "tasks/create.html"
